9d741302ae6baef879d93af034b91433476f4f8c
Godhead42 <tilesjan2005@gmail.com>
2023-11-10 13:40:38 +0600

    Initial commit

---
+++ decorator.py
@@ -0,0 +1,9 @@
+# decorator.py
+
+from abc import ABC, abstractmethod
+
+# Определяем интерфейс декоратора
+class ClothingDecorator(ABC):
+    @abstractmethod
+    def get_clothing_list(self):
+        pass
